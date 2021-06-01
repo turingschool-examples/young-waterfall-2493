7f6aeb4d0d6e91a78d3f2a8d50a5dda29da4cf1f
deebot10 <dee1jhill@gmail.com>
2021-06-01 10:08:39 -0600

    Set up for User story 1

---
+++ spec/features/studio/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'studio show page' do
+  before(:each) do 
+      
+  end  
+    
+  #User Story 1  
+  it 'shows a studio attributes ' do
+  end    
+
+  xit 'shows the titles of all the studio movies' do
+  end
+end
