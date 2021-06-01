cd4e8c9f07a267e4852b96b4766a1ec9a217a4d5
deebot10 <dee1jhill@gmail.com>
2021-06-01 10:21:57 -0600

    Created Studio controller

---
+++ app/controllers/studio_controller.rb
@@ -0,0 +1,5 @@
+class StudioController < ApplicationController
+  def show
+      
+  end    
+end
--- spec/features/studio/show_spec.rb
+++ spec/features/studio/show_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'studio show page' do
   before(:each) do 
-      
+  @studio = Studio.create!(name: 'Universal Studios', location: 'Hollywood') 
+  @movie_1 = @studio.movies.create!(title: 'Sound of Metal', creation_year: 2020)   
+  @movie_2 = @studio.movies.create!(title: 'The Dark Knight', creation_year: 2010)   
   end  
     
   #User Story 1  
   it 'shows a studio attributes ' do
+    visit "/studio/#{@studio.id}"   
+
+    expect(page).to have_content(@studio.name)
+    expect(page).to have_content(@studio.location)
   end    
 
   xit 'shows the titles of all the studio movies' do
