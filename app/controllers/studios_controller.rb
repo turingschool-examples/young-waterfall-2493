4978f5ff519a9b659a2d627d2846404b3a0e6e9c
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-06-01 11:45:32 -0400

    Create view for user story 1, studio show page

---
+++ app/controllers/studios_controller.rb
@@ -0,0 +1,6 @@
+class StudiosController < ApplicationController
+  def show
+    @studio = Studio.find(params[:id])
+    @movies = @studio.movies
+  end 
+end
--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 RSpec.describe 'studio show page' do
     before(:each) do
       @studio = Studio.create!(name: "Whatever Studio", location: "Anywhere, World")
-      @movie_1 = Movie.create!(title: "Movie One", creation_year: 2020, gnere: "Comedy")
-      @movie_2 = Movie.create!(title: "Movie Two", creation_year: 2018, genre: "Thriller")
-      @movie_3 = Movie.create!(title: "Movie Three", creation_year: 2010, genre: "Romance")
+      @movie_1 = Movie.create!(title: "Movie One", creation_year: 2020, genre: "Comedy", studio_id: @studio.id)
+      @movie_2 = Movie.create!(title: "Movie Two", creation_year: 2018, genre: "Thriller", studio_id: @studio.id)
+      @movie_3 = Movie.create!(title: "Movie Three", creation_year: 2010, genre: "Romance", studio_id: @studio.id)
     end
 
     it 'shows the name and location of the studio and the titles of all of its movies' do
-      @studio.movies << @movie_1
-      @studio.movies << @movie_2
-      @studio.movies << @movie_3
     visit "/studios/#{@studio.id}"
 
     expect(page).to have_content("#{@studio.name}")
