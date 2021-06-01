7ce75fd2826187d8fe8966fe9c1bb20516d398f7
Zach Green <zachjamesgreen@gmail.com>
2021-06-01 10:41:04 -0600

    route set up for add_actor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def show
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find params[:id]
+  end
+
+  def add_actor
+    movie = Movie.find params[:id]
+    movie.actors << Actor.find params[:actor]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/studios/:id', to: 'studios#show'
   get '/movies/:id', to: 'movies#show'
+  patch '/movies/:id/add_actor', to: 'movies#add_actor'
 end
