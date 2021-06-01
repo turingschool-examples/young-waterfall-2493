7ea2b1dc13c4bc6ac4bceb32b0ac955b60a5f951
Zach T <ztrokey@gmail.com>
2021-06-01 11:58:35 -0600

    test for currently working actors

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Movie do
     it {should have_many :actors}
     it { should have_many(:actors).through(:movie_actors) }
   end
+  it 'only shows actors that are currently working' do
+    studio_1 = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    movie_1 = studio_1.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+    actor_1 = movie_1.actors.create!(name: 'Chris P. Bacon', age:54, currently_working: true)
+    actor_2 = movie_1.actors.create!(name: 'Anita Nappe', age:87, currently_working: false)
+    actor_3 = movie_1.actors.create!(name: 'Crystall Ball', age:34, currently_working: true)
+
+    expect(movie_1.actors.all).to eq([actor_3, actor_1])
+  end
 end
