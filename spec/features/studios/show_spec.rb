f31ea4273322354903707a57ed7551bdb57eda39
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-01 09:25:28 -0600

    Green user story 1 test

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'studio show page' do
     it 'shows the name location and movie titles' do
       studio = Studio.create(name: 'Paramount', location: 'Iowa')
       movie_1 = studio.movies.create(title: 'Man man', creation_year: '1876', genre: 'horror')
-      movie_2 = studio.movies.create(title: 'Future Shock', creation_year: '2021', genre: 'comedy'
+      movie_2 = studio.movies.create(title: 'Future Shock', creation_year: '2021', genre: 'comedy')
       visit "/studios/#{studio.id}"
 
       expect(page).to have_content(studio.name)
