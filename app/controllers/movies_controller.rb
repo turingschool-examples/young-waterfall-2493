9f873e9ae40fc152bf921e969a6b2a5cac8f935b
Suzie K <suziekim.dev@gmail.com>
2021-06-01 12:38:56 -0400

    Complete User Story 3

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,7 @@
+class MoviesController < ApplicationController
+  
+  def show
+    @movie = Movie.find(params[:id])
+    @actors = @movie.actors
+  end
+end
---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'studio show page' do
+  describe 'display' do
+    before :each do 
+      @studio = Studio.create!(name: "Colby Studios", location: "Boston")
+      @howl = @studio.movies.create!(title: "Howl's Moving Castle", creation_year: 2004 , genre: 'Fantasy')
+      @jacob = @howl.actors.create!(name: "Jacob Ordy", age: 13, currently_working: true)
+      @allison = @howl.actors.create!(name: "Allison Ray", age: 24, currently_working: true)
+      @snow_white = @studio.movies.create!(title: 'Snow White and the Seven Dwarves', creation_year: 1960 , genre: 'Fairytale')
+      @bill = @snow_white.actors.create!(name: "Bill Wise", age: 63, currently_working: true)
+    end
+
+    it 'show movie and attributes' do
+      visit "/movies/#{@howl.id}"
+
+      expect(page).to have_content("#{@howl.title}")
+      expect(page).to have_content("Creation Year: #{@howl.creation_year}")
+      expect(page).to have_content("Genre: #{@howl.genre}")
+    end
+
+    it 'lists all actors in movie' do
+      visit "/movies/#{@howl.id}"
+
+      expect(page).to have_content("All Actors:")
+      expect(page).to have_content("* #{@jacob.name}")
+      expect(page).to have_content("* #{@allison.name}")
+      expect(page).to_not have_content("* #{@bill.name}")
+    end
+
+  end 
+end
