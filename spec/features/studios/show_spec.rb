d2cadd322d107db475285f970cb94128cc0b8295
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-01 10:18:40 -0600

    Green user story 2 tests

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe 'studio show page' do
       movie_2 = studio.movies.create(title: 'Future Shock', creation_year: '2021', genre: 'comedy')
       actor_1 = Actor.create(name: 'Marison Hord', age: 26, currently_working: true)
       actor_2 = Actor.create(name: 'MZuko', age: 16, currently_working: true)
+      actor_3 = Actor.create(name: 'Ben Fold', age: 45, currently_working: true)
       MovieActor.create(movie: movie_1, actor: actor_1)
       MovieActor.create(movie: movie_2, actor: actor_2)
 
@@ -26,6 +27,7 @@ RSpec.describe 'studio show page' do
 
       expect(page).to have_content(actor_1.name)
       expect(page).to have_content(actor_2.name)
+      expect(page).to_not have_content(actor_3.name)
     end
     it 'shows only lists actors once' do
       studio = Studio.create(name: 'Paramount', location: 'Iowa')
@@ -44,16 +46,30 @@ RSpec.describe 'studio show page' do
     it 'shows only lists actors who are currently working' do
       studio = Studio.create(name: 'Paramount', location: 'Iowa')
       movie_1 = studio.movies.create(title: 'Man man', creation_year: '1876', genre: 'horror')
-      movie_2 = studio.movies.create(title: 'Future Shock', creation_year: '2021', genre: 'comedy')
       actor_1 = Actor.create(name: 'Marison Hord', age: 26, currently_working: false)
-      actor_2 = Actor.create(name: 'MZuko', age: 16, currently_working: true)
       MovieActor.create(movie: movie_1, actor: actor_1)
-      MovieActor.create(movie: movie_2, actor: actor_1)
-      MovieActor.create(movie: movie_2, actor: actor_2)
+
 
       visit "/studios/#{studio.id}"
 
       expect(page).to_not have_content(actor_1.name)
+    end
+    it 'lists actors by age from oldest' do
+      studio = Studio.create(name: 'Paramount', location: 'Iowa')
+      movie_1 = studio.movies.create(title: 'Man man', creation_year: '1876', genre: 'horror')
+      movie_2 = studio.movies.create(title: 'Future Shock', creation_year: '2021', genre: 'comedy')
+      actor_1 = Actor.create(name: 'Marison Hord', age: 26, currently_working: true)
+      actor_2 = Actor.create(name: 'MZuko', age: 16, currently_working: true)
+      actor_3 = Actor.create(name: 'Ben Fold', age: 45, currently_working: true)
+      MovieActor.create(movie: movie_1, actor: actor_1)
+      MovieActor.create(movie: movie_2, actor: actor_2)
+      MovieActor.create(movie: movie_2, actor: actor_3)
+
+
+      visit "/studios/#{studio.id}"
+
+      expect(actor_3.name).to appear_before(actor_1.name)
+      expect(actor_1.name).to appear_before(actor_2.name)
     end
   end
 end
