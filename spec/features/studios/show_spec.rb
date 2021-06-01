4c3f065822894d25e62b9500222ab3f984d69410
Suzie K <suziekim.dev@gmail.com>
2021-06-01 11:46:01 -0400

    Red lists titles of studio movies

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -10,13 +10,27 @@ require 'rails_helper'
 
 RSpec.describe 'studio show page' do
   describe 'display' do
+    before :each do 
+      @studio = Studio.create!(name: "Colby Studios", location: "Boston")
+      @raiders = @studio.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981 , genre: 'Action/Adventure')
+      @howl = @studio.movies.create!(title: "Howl's Moving Castle", creation_year: 2004 , genre: 'Fantasy')
+      @snow_white = @studio.movies.create!(title: 'Snow White and the Seven Dwarves', creation_year: 1960 , genre: 'Fairytale')
+    end
+
     it 'show studio and attributes' do
-      studio = Studio.create!(name: "Colby Theater", location: "Boston")
+      visit "/studios/#{@studio.id}"
 
-      visit "/studios/#{studio.id}"
+      expect(page).to have_content("#{@studio.name}")
+      expect(page).to have_content("Location: #{@studio.location}")
+    end
 
-      expect(page).to have_content("#{studio.name}")
-      expect(page).to have_content("Location: #{studio.location}")
+    it 'titles of studio movies' do
+      visit "/studios/#{@studio.id}"
+
+      expect(page).to have_content("All Movies:")
+      expect(page).to have_content("* #{@raiders.title}")
+      expect(page).to have_content("* #{@howl.title}")
+      expect(page).to have_content("* #{@snow_white.title}")
     end
   end 
 end
