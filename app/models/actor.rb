69771bbcd86a5cd65e273c0cb1a8fc6341158c59
deebot10 <dee1jhill@gmail.com>
2021-06-01 09:51:15 -0600

    Got belongs_to relationship to pass

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,4 @@
 class Actor < ApplicationRecord
+  has_many :blockbusters
+  has_many :movies, through: :blockbusters    
 end
--- app/models/blockbuster.rb
+++ app/models/blockbuster.rb
@@ -1,2 +1,4 @@
 class Blockbuster < ApplicationRecord
+  belongs_to :actor
+  belongs_to :movie    
 end
